68da10778bfbde2cd53d6ff2d2d7a4812276760f
ybb <34953126@qq.com>
2019-08-18 16:40:54 +0800

    练习

---
+++ com/test/day1_循环_20190818.py
@@ -0,0 +1,17 @@
+
+#循环
+for  ever_letter in 'hello world!' :
+    print(ever_letter)
+
+for num in range(1,11):#不包含11
+    print(str(num)+' + 1 =',num +1)
+
+
+songslist = ['Holy Diver','Thunderstruck','Reble Reble']
+for song in songslist:
+    if song == 'Holy Diver':
+        print(song,'- Dio')
+    elif song == 'Thunderstruck':
+        print(song,'-AC -DC')
+    elif song == 'Rebel Rebel':
+        print(song, ' - David Bowie')
