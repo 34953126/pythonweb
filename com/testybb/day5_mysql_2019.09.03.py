1639e9871b2f9a222b0b9f4416d3d3af95370386
admin <34953126@qq.com>
2019-09-04 14:33:40 +0800

    Python3 mysql-connector

--- com/testybb/day5_mysql_2019.09.03.py
+++ com/testybb/day5_mysql_2019.09.03.py
@@ -2,25 +2,51 @@
 # 鼠标选中mysql-connector-python--->点击左下角Install Package
 
 
-
-
 import mysql.connector
-
 # 创建数据库连接
-# mydb = mysql.connector.connect(
-#     host="localhost",   #数据库主机地址
-#     user="test",        #数据库用户名
-#     password="test",       #数据库密码
-#     port="3306",
-#     database="test"
-# )
-
-conn = mysql.connector.connect(
-    host='localhost',
-    user='test',
-    password='test',
-    auth_plugin = 'mysql_native_password'
+mydb = mysql.connector.connect(
+    host="localhost",   #数据库主机地址
+    user="root",        #数据库用户名
+    password="123",     #数据库密码
+    port="3340",        #数据库端口
+    database="test"     #数据库名
 )
+print(mydb)
 
-print(conn)
+#创建表
+mycursor = mydb.cursor()
+#mycursor.execute("CREATE TABLE sites (name VARCHAR(255), url VARCHAR(255))") #（只需运行一次）
 
+#查看所有表
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+    print(x)
+
+#设置主键
+#mycursor.execute(" ALTER TABLE sites ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY ")#（只需运行一次）
+
+#插入数据
+# sql = "insert into sites (name,url) values (%s,%s)"
+# val = ("baidu","http://www.baidu.com")
+# mycursor.execute(sql,val)
+# mydb.commit()
+# print(mycursor.rowcount,"记录插入成功")
+
+#批量插入
+sql = "INSERT INTO sites (name, url) VALUES (%s, %s)"
+val = [
+    ('Google', 'https://www.google.com'),
+    ('Github', 'https://www.github.com'),
+    ('Taobao', 'https://www.taobao.com'),
+    ('stackoverflow', 'https://www.stackoverflow.com/')
+]
+mycursor.executemany(sql, val)
+mydb.commit()  # 数据表内容有更新，必须使用到该语句
+print(mycursor.rowcount, "记录插入成功。")
+print("Id:",mycursor.lastrowid)#获取id
+
+#查询数据
+mycursor.execute("select * from sites")
+myresult = mycursor.fetchall()
+for y in myresult:
+    print(y)
